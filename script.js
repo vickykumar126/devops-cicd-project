e2c3da556d115be788d763411b3b3f20c5334dd0
vickykumar126 <vickykumar0143vkvk@gmail.com>
2026-09-02 12:23:04 +0530

    Initial website

---
+++ script.js
@@ -0,0 +1,13 @@
+const themeBtn = document.getElementById("themeBtn");
+
+themeBtn.addEventListener("click", () => {
+
+    document.body.classList.toggle("light");
+
+    if (document.body.classList.contains("light")) {
+        themeBtn.textContent = "☀️";
+    } else {
+        themeBtn.textContent = "🌙";
+    }
+
+});
